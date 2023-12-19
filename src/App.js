b6235cd4324c3f410879deefd924964f32ccf38a
Paramjot Singh <paramjotdhiman5@gmail.com>
2023-12-20 02:12:38 +0530

    feat: Move and resize shapes and undo/ redo functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,139 @@
-import logo from './logo.svg';
 import './App.css';
-import { useLayoutEffect, useState } from 'react';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { act } from 'react-dom/test-utils';
 import rough from 'roughjs/bundled/rough.esm';
 
-const generator = rough.generator();
+const generator =  rough.generator();
 
-function createElement(x1, y1, x2, y2, elementType){
+const Shapes = {
+  LINE: 'line',
+  RECTANGLE: 'rectangle'
+}  
+
+function createElement(id, x1, y1, x2, y2, type){
   const roughElement =
-  elementType === "line" 
+  type === "line" 
     ? generator.line(x1, y1, x2, y2)
     : generator.rectangle(x1, y1, x2-x1, y2-y1);
 
-  return {x1, y1, x2, y2, roughElement};
+  return {id, x1, y1, x2, y2, type, roughElement};
+}
+
+const nearPoint = (x, y, x1, y1, name) => {
+  return Math.abs(x - x1) < 5 && Math.abs(y - y1) < 5 ? name : null;
+}
+
+const isWithinElement = (x, y, element) =>{
+  const {type, x1, x2, y1, y2} = element;
+  if(type === "rectangle"){    
+    const topLeft = nearPoint(x, y, x1, y1, "tl");
+    const topRight = nearPoint(x, y, x2, y1, "tr");
+    const bottomLeft = nearPoint(x, y, x1, y2, "bl");
+    const bottomRight = nearPoint(x, y, x2, y2, "br");
+    const inside = x >= x1 && x <= x2 && y >= y1 && y <= y2 ? "inside": null;
+    
+    return topLeft || topRight || bottomRight || bottomLeft || inside;
+  }else{
+    const a = {x: x1, y: y1};
+    const b = {x: x2, y: y2};
+    const c = {x, y};
+    const offset = distance(a, b) - (distance(a, c) + distance(b,c));
+    const start = nearPoint(x, y, x1, y1, "start");
+    const end = nearPoint(x, y, x2, y2, "end");
+    const inside = Math.abs(offset) < 1 ? "inside": null;
+    return start || end || inside;
+  }
+}
+
+const distance = (a, b) =>{
+  return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+}
+
+const getElementAtPosition = (x, y, elements) =>{
+  return elements.map(element =>(
+    {...element, position: isWithinElement(x, y, element)}
+    )).find(element => element.position !== null); 
+}
+
+const cursorForPosition = position =>{
+  switch(position){
+    case "tl":
+    case "br":
+    case "start":
+    case "end": 
+      return "nwse-resize";
+    case "tr":
+    case "bl":
+      return "nesw-resize";
+    default:
+      return "move";
+  }
+}
+
+const resizedCoordinates = (clientX, clientY, position, coordinates) =>{
+  const {x1, y1, x2, y2} = coordinates;
+  switch(position){
+    case "tl":
+    case "start":
+      return{x1: clientX, y1: clientY, x2, y2};
+    case "tr":
+      return{x1, y1: clientY, x2: clientX, y2};
+    case "bl":
+      return{x1: clientX, y1, x2, y2: clientY};
+    case "br":
+    case "end":
+      return{x1, y1, x2:  clientX, y2: clientY};
+    default:
+      return null;
+  }
+}
+
+const adjustElementCoordinates = element => {
+  const { type, x1, y1, x2, y2 } = element;
+  if (type === "rectangle") {
+    const minX = Math.min(x1, x2);
+    const maxX = Math.max(x1, x2);
+    const minY = Math.min(y1, y2);
+    const maxY = Math.max(y1, y2);
+    return { x1: minX, y1: minY, x2: maxX, y2: maxY };
+  } else {
+    if (x1 < x2 || (x1 === x2 && y1 < y2)) {
+      return { x1, y1, x2, y2 };
+    } else {
+      return { x1: x2, y1: y2, x2: x1, y2: y1 };
+    }
+  }
+};
+
+const useHistory = initialState =>{
+  const[index, setIndex] = useState(0);
+  const[history, setHistory] = useState([initialState]);  
+
+  const setState = (action, overwrite = false) =>{
+    const newState = typeof action === "function" ? action(history[index]) : action;
+    
+    if(overwrite){
+      const historyCopy = [...history];
+      historyCopy[index] = newState;
+      setHistory(historyCopy);
+    }else{
+      const updatedState = [...history].slice(0, index + 1);
+      setHistory(prevState => [...updatedState, newState]);
+      setIndex(prevState => prevState + 1);
+    }
+  };
+
+  const undo = () => { index > 0 && setIndex(prevState => prevState - 1) }
+  const redo = () => { index < history.length - 1 && setIndex(prevState => prevState + 1)}
+
+  return [history[index], setState, undo, redo];
 }
 
 function App() {
-
-  const[elements, setElements] = useState([]);
-  const[drawing, setDrawing] = useState(false);
-  const[elementType, setElementType] = useState('line');
+  const[elements, setElements, undo, redo] = useHistory([]);
+  const[action, setAction] = useState("none");
+  const[tool, setTool] = useState("line");
+  const[selectedElement, setSelectedElement] = useState(null)
 
   useLayoutEffect(() =>{
     const canvas = document.getElementById("canvas");
@@ -30,28 +145,100 @@ function App() {
     elements.forEach(({roughElement}) => roughCanvas.draw(roughElement));
   }, [elements]);
 
+  useEffect(() =>{
+    const undoRedoFunction = event =>{
+      if((event.metaKey || event.ctrlKey) && event.key === "z"){
+          undo();        
+      }
+      else if((event.metaKey || event.ctrlKey) && event.key === "y"){
+        redo();
+      }
+    };
+
+    document.addEventListener("keydown", undoRedoFunction);
+
+    return () => {
+      document.removeEventListener("keydown", undoRedoFunction);
+    };
+  }, [undo, redo]);
+
+  const updateElement = (index, x1, y1, x2, y2, tool) =>{
+    const updatedElement = createElement(index, x1, y1, x2, y2, tool);    
+      
+    const elementsCopy = [...elements];
+    elementsCopy[index] = updatedElement;
+    setElements(elementsCopy, true);
+  }
+
   const handleMouseDown = (event) =>{
-    setDrawing(true);
     const {clientX, clientY} = event;
-    const element = createElement(clientX, clientY, clientX, clientY, elementType);
-    setElements( prevState => [...prevState, element]);
+    if(tool === "selection"){      
+      const element = getElementAtPosition(clientX, clientY, elements);
+      if(element){
+        const offsetX = clientX - element.x1;
+        const offsetY = clientY - element.y1;
+        setSelectedElement({...element, offsetX, offsetY});
+        setElements(prevState => prevState);
+
+        if(element.position === "inside"){
+          setAction("moving");
+        }          
+        else{
+         setAction("resizing");
+        }
+      }
+    }else{             
+      const id = elements.length;
+      const element = createElement(id, clientX, clientY, clientX, clientY, tool);
+      setElements( prevState => [...prevState, element]);
+      setSelectedElement(element);
+      setAction("drawing");
+    }
   };
 
+
+
   const handleMouseUp = (event) =>{
-    setDrawing(false);
+
+    if(selectedElement){
+      const index = selectedElement.id;
+      const {id, type} = elements[index];
+      if(action === 'drawing' || action === "resizing"){
+        const {x1, y1, x2, y2} = adjustElementCoordinates(elements[index]);
+        updateElement(id, x1, y1, x2, y2, type);
+      }
+    }   
+
+    setAction("none");
+    setSelectedElement(null);
   };
   
   const handleMouseMove = (event) =>{
-    if(!drawing) return;
-
     const {clientX, clientY} = event;
-    const index = elements.length - 1;    
-    const {x1, y1} = elements[index];
-    const updatedElement = createElement(x1, y1, clientX, clientY, elementType);    
+
+    if(tool === "selection"){
+      const element = getElementAtPosition(clientX, clientY, elements);
+      event.target.style.cursor = element ? cursorForPosition(element.position) : "default";
+    }    
     
-    const elementsCopy = [...elements];
-    elementsCopy[index] = updatedElement;
-    setElements(elementsCopy);
+    if(action === "drawing"){            
+      const index = elements.length - 1;    
+      const {x1, y1} = elements[index];
+      updateElement(index, x1, y1, clientX, clientY, tool);      
+    }
+    else if(action === "moving"){
+      const {id, x1, y1, x2, y2, type, offsetX, offsetY} = selectedElement;
+      const width = x2 - x1;
+      const height = y2 - y1;
+      const nexX1 = clientX - offsetX;
+      const nexY1 = clientY - offsetY;
+      updateElement(id, nexX1, nexY1, nexX1 + width, nexY1 + height, type)
+    }
+    else if(action === "resizing"){
+      const {id, type, position, ...coordinates } = selectedElement;
+      const {x1, y1, x2, y2} = resizedCoordinates(clientX, clientY, position, coordinates);
+      updateElement(id, x1, y1, x2, y2, type);
+    }
   };
 
   return (
@@ -59,21 +246,32 @@ function App() {
     <div style={{position: "fixed"}}>
       <input
         type='radio'
+        id='selection'
+        checked = {tool === 'selection'}
+        onChange={() => setTool("selection")}
+        value={"selection"}
+      /><label htmlFor='selection'>Selection</label>
+      <input
+        type='radio'
         id='line'
-        checked = {elementType === 'line'}
-        onChange={() => setElementType("line")}
+        checked = {tool === 'line'}
+        onChange={() => setTool("line")}
         value={"line"}
       />
       <label htmlFor='line'>Line</label>
       <input
         type='radio'
         id='rectangle'
-        checked = {elementType === 'rectangle'}
-        onChange={() => setElementType("rectangle")}
+        checked = {tool === 'rectangle'}
+        onChange={() => setTool("rectangle")}
         value={"rectangle"}
       />
       <label htmlFor='rectangle'>Rectangle</label>
     </div>    
+    <div style={{position: "fixed", bottom:0, padding: 10}}>
+      <button onClick={undo}>Undo</button>
+      <button onClick={redo}>Redo</button>
+    </div>
     <div>
     <canvas id='canvas' height={window.innerHeight} 
     width={window.innerWidth} 
